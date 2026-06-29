3a20283d1d76ff2b50a69a7a4b7928fd0bbb9e1c
DarshanKS22 <darshanks22042003@gmail.com>
2026-06-29 14:44:01 +0530

    Initial Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Webhook Demo Project");
+
+        System.out.println("Build Triggered Successfully");
+
+    }
+
+}
